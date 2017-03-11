83eee98bdc353ce9f8ead37dc8a867210123eb89
Byron Wang <wangshilian@gmail.com>
2017-03-11 09:16:39 +0800

    change yesno

--- src/main/java/nebula/define/YesNo.java
+++ src/main/java/nebula/define/YesNo.java
@@ -1,6 +1,9 @@
 package nebula.define;
 
 import nebula.define.Boolean;
+import static nebula.define.Runtime.*;
 
 public interface YesNo extends Boolean {
+	static public YesNo Yes = _C(true);
+	static public YesNo No = _C(false);
 };
